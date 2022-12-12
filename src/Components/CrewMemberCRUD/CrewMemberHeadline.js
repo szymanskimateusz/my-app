0496d77e9e45e9923a6f839df7cc0f6dc0b6d7d4
szymanskimateusz <szymanskimateusz@users.noreply.github.com>
2022-12-12 13:26:15 +0100

    pls work

--- src/Components/CrewMemberCRUD/CrewMemberHeadline.js
+++ src/Components/CrewMemberCRUD/CrewMemberHeadline.js
@@ -3,11 +3,11 @@ import React from "react";
 export default function CrewMemberHeadline(props) {
     return (
         <div className="memberlist-row">
-            <div className="medium">Name</div> 
+            <div className="medium">Name</div>
             <div className="thick">Last Name</div>
             <div className="thick">Fav Trick</div>
             <div className="thick">Dream Trick</div>
-            <div className="thin">Age</div> 
+            <div className="thin">Age</div>
             <div className="thin">YT</div>
             <div className="thin">IG</div>
             <div className="thin">Img</div>
